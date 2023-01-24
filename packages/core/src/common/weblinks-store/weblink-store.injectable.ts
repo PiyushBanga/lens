79b16baef4b56a6dcfd54715d7c61f9e502f99ec
Sebastian Malton <sebastian@malton.name>
2023-01-24 10:30:36 -0500

    Move WeblinkStore to new format
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- packages/core/src/common/weblinks-store/weblink-store.injectable.ts
+++ packages/core/src/common/weblinks-store/weblink-store.injectable.ts
@@ -3,16 +3,8 @@
  * Licensed under MIT License. See LICENSE in root directory for more information.
  */
 import { getInjectable } from "@ogre-tools/injectable";
-import directoryForUserDataInjectable from "../app-paths/directory-for-user-data/directory-for-user-data.injectable";
-import { baseStoreIpcChannelPrefixesInjectionToken } from "../base-store/channel-prefix";
-import { shouldBaseStoreDisableSyncInIpcListenerInjectionToken } from "../base-store/disable-sync";
+import createBaseStoreInjectable from "../base-store/create-base-store.injectable";
 import storeMigrationsInjectable from "../base-store/migrations.injectable";
-import { persistStateToConfigInjectionToken } from "../base-store/save-to-file";
-import getConfigurationFileModelInjectable from "../get-configuration-file-model/get-configuration-file-model.injectable";
-import loggerInjectable from "../logger.injectable";
-import getBasenameOfPathInjectable from "../path/get-basename.injectable";
-import { enlistMessageChannelListenerInjectionToken } from "../utils/channel/enlist-message-channel-listener-injection-token";
-import { sendMessageToChannelInjectionToken } from "../utils/channel/message-to-channel-injection-token";
 import { weblinkStoreMigrationInjectionToken } from "./migration-token";
 import weblinksStoreMigrationVersionInjectable from "./migration-version.injectable";
 import { WeblinkStore } from "./weblink-store";
@@ -20,17 +12,9 @@ import { WeblinkStore } from "./weblink-store";
 const weblinkStoreInjectable = getInjectable({
   id: "weblink-store",
   instantiate: (di) => new WeblinkStore({
-    directoryForUserData: di.inject(directoryForUserDataInjectable),
-    getConfigurationFileModel: di.inject(getConfigurationFileModelInjectable),
-    logger: di.inject(loggerInjectable),
     storeMigrationVersion: di.inject(weblinksStoreMigrationVersionInjectable),
     migrations: di.inject(storeMigrationsInjectable, weblinkStoreMigrationInjectionToken),
-    getBasenameOfPath: di.inject(getBasenameOfPathInjectable),
-    ipcChannelPrefixes: di.inject(baseStoreIpcChannelPrefixesInjectionToken),
-    persistStateToConfig: di.inject(persistStateToConfigInjectionToken),
-    enlistMessageChannelListener: di.inject(enlistMessageChannelListenerInjectionToken),
-    shouldDisableSyncInListener: di.inject(shouldBaseStoreDisableSyncInIpcListenerInjectionToken),
-    sendMessageToChannel: di.inject(sendMessageToChannelInjectionToken),
+    createBaseStore: di.inject(createBaseStoreInjectable),
   }),
 });
 
