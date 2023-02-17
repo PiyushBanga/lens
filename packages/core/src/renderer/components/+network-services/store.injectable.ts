613d25e4b8dcb0dd7a7c8f141c57ce980fb0f7c3
Antti Lustila <antti.lustila@luotocompany.fi>
2023-02-17 16:19:24 +0200

    Enable Vulnerabilities display in Service details

--- packages/core/src/renderer/components/+network-services/store.injectable.ts
+++ packages/core/src/renderer/components/+network-services/store.injectable.ts
@@ -4,6 +4,7 @@
  */
 import { getInjectable } from "@ogre-tools/injectable";
 import assert from "assert";
+import getPodsByOwnerIdInjectable from "../+workloads-pods/get-pods-by-owner-id.injectable";
 import { kubeObjectStoreInjectionToken } from "../../../common/k8s-api/api-manager/kube-object-store-token";
 import serviceApiInjectable from "../../../common/k8s-api/endpoints/service.api.injectable";
 import loggerInjectable from "../../../common/logger.injectable";
@@ -19,6 +20,7 @@ const serviceStoreInjectable = getInjectable({
     const api = di.inject(serviceApiInjectable);
 
     return new ServiceStore({
+      getPodsByOwnerId: di.inject(getPodsByOwnerIdInjectable),
       context: di.inject(clusterFrameContextForNamespacedResourcesInjectable),
       logger: di.inject(loggerInjectable),
     }, api);
--- packages/core/src/renderer/components/+network-services/store.ts
+++ packages/core/src/renderer/components/+network-services/store.ts
@@ -2,9 +2,23 @@
  * Copyright (c) OpenLens Authors. All rights reserved.
  * Licensed under MIT License. See LICENSE in root directory for more information.
  */
-
+import type { KubeObjectStoreDependencies, KubeObjectStoreOptions } from "../../../common/k8s-api/kube-object.store";
 import { KubeObjectStore } from "../../../common/k8s-api/kube-object.store";
 import type { Service, ServiceApi } from "../../../common/k8s-api/endpoints/service.api";
+import type { GetPodsByOwnerId } from "../+workloads-pods/get-pods-by-owner-id.injectable";
+import type { Pod } from "../../../common/k8s-api/endpoints";
+
+interface Dependencies extends KubeObjectStoreDependencies {
+  getPodsByOwnerId: GetPodsByOwnerId;
+}
 
 export class ServiceStore extends KubeObjectStore<Service, ServiceApi> {
+
+  constructor(protected readonly dependencies: Dependencies, api: ServiceApi, opts?: KubeObjectStoreOptions) {
+    super(dependencies, api, opts);
+  }
+
+  getChildPods(service: Service): Pod[] {
+    return this.dependencies.getPodsByOwnerId(service.getId());
+  }
 }
